80ae4918d9f13df5fb005b2cdead36cdbed30e8b
G85lba <gulbahram.karazhakova@gmail.com>
2024-05-03 17:31:57 +0500

    first commit

---
+++ app/src/main/java/com/example/lab2/Celebrity.kt
@@ -0,0 +1,11 @@
+package com.example.lab2
+
+data class Celebrity(
+    val name: String?,
+    val netWorth: Long? ,
+    val gender: String?,
+    val nationality: String? ,
+    val occupation: List<String>? ,
+    val height: Double? ,
+    val birthday: String? ,
+)
